ef56d4d38813c818df43126eb9a6f0b0b5cd4f35
COBOide <clement.byrnes@oide.ie>
2026-05-01 16:50:53 +0100

    Update Finding a port.py

--- Year_1/PLE1_Activities/Additional_Resources_MB_Python/Finding a port.py
+++ Year_1/PLE1_Activities/Additional_Resources_MB_Python/Finding a port.py
@@ -1,5 +1,5 @@
 # Event:PLE 1 
-# Purpose: To find an active port that will allow serial communcation with a MicroBit
+# Purpose: To find an active port that will allow serial communcation with a micro:bit
 # LCCS Team
 
 from microfs import*
